63e132b0ba8cf8aac847e02416afee0552e7ee4c
Luke O'Malley <Lukeomalley95@gmail.com>
2019-08-27 15:01:14 -0400

    installed graphql and generated graphql objects for models

---
+++ top-dog-api/app/graphql/top_dog_api_schema.rb
@@ -0,0 +1,4 @@
+class TopDogApiSchema < GraphQL::Schema
+  mutation(Types::MutationType)
+  query(Types::QueryType)
+end
---
+++ top-dog-api/app/graphql/types/rating_type.rb
@@ -0,0 +1,4 @@
+module Types
+  class RatingType < Types::BaseObject
+  end
+end
